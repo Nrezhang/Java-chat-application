d40d1f07ef29161c6476e1f539933c2f45d3b748
nrezhang <henryszhang83@gmail.com>
2024-05-01 21:54:16 -0400

    create file fucntion

--- src/DirectoryManager.java
+++ src/DirectoryManager.java
@@ -43,17 +43,45 @@ public class DirectoryManager {
     }
 
     public File makeNewFile(String folderName, String fileName) {
-        path = "app-data" + folderName + "/" + fileName;
-        return new File(path);
+        path = "app-data/" + folderName + "/" + fileName;
+        File newFile = new File(path);
+        try {
+            if (newFile.createNewFile()) {
+                System.out.println("File created: " + newFile.getAbsolutePath());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.err.println("Error creating file: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return newFile;
+        
     }
 
     //TODO
     public String getFileContent(String filename) {
-        return "";
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
     }
 
     //TODO
-    public boolean setFileContent(String filename) {
-        return false;
+    public boolean setFileContent(String filename, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(content);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
--- src/EditNoteGUI.java
+++ src/EditNoteGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Class creates GUI to provide an interface for the user to create, edit, and save files in a given subdirectory
@@ -9,22 +11,34 @@ public class EditNoteGUI extends JFrame {
 
     private String folderName;
     private String fileName;
+    private DirectoryManager directoryManager;
 
     public EditNoteGUI(String directoryPath) {
         folderName = directoryPath;
         setTitle("Notes App / " + folderName);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        directoryManager = new DirectoryManager();
 
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         JMenuItem openItem = new JMenuItem("Save");
         JMenuItem exitItem = new JMenuItem("Exit");
+        JMenuItem addItem = new JMenuItem("New");
         fileMenu.add(openItem);
+        fileMenu.addSeparator();
+        fileMenu.add(addItem);
         fileMenu.addSeparator();
         fileMenu.add(exitItem);
         menuBar.add(fileMenu);
         setJMenuBar(menuBar);
 
+        addItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createNewFile();
+            }
+        });
 
         String[] columns = {folderName};
         String[][] data = {{"Note 1"}, {"Note 2"}, {"Note 3"}};
@@ -44,6 +58,12 @@ public class EditNoteGUI extends JFrame {
     }
 
     //for testing
+    private void createNewFile(){
+        String fileName = JOptionPane.showInputDialog("Enter file name:");
+        if (fileName != null && !fileName.isEmpty()) {
+            directoryManager.makeNewFile(folderName, fileName);
+        }
+    }
     //TODO remove
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new EditNoteGUI("test"));
